2c5127c28265ea00525eb4a2e7cd1ca2e5b2fc0b
Rebekah <figueroarebekah@gmail.com>
2022-12-13 15:40:27 -0600

    commit tree 4d0016e067cd864864ff82abe1fc3e38f23c41c9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity) {
+  return `This Saturday, I want to ${activity || "roller-skate"}!`;
+
+  //   return activity
+  //     ? `This Saturday, I want to ${activity}!`
+  //     : "This Saturday, I want to roller-skate!";
+}
+
+function mondayWork(work) {
+  return `This Monday, I will ${work || "go to the office"}.`;
+}
+
+function wrapAdjective(flair) {
+  return function (adjective) {
+    return `You are ${flair}${adjective || "special"}${flair}!`;
+  };
+}
